c042a7ffaa422d236b3013a0377a5949a6a63212
diegopacheco <diego.pacheco.it@gmail.com>
2024-03-12 00:33:52 -0700

    panda basics

---
+++ pocs/pandas-basics/src/main.py
@@ -0,0 +1,8 @@
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+   "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+   "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+   "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
+import pandas as pd
+brics = pd.DataFrame(dict)
+print(brics)
