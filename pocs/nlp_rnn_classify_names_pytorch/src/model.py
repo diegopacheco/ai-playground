21992e783d8f4232d3e25e757bf28baf49cd827c
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-01 03:04:47 -0700

    pytorch poc - NLP / RNN name classifications

--- pocs/nlp_rnn_classify_names_pytorch/src/model.py
+++ pocs/nlp_rnn_classify_names_pytorch/src/model.py
@@ -5,19 +5,23 @@ from torch.autograd import Variable
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
-
         self.hidden_size = hidden_size
 
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.i2h = nn.Linear(in_features=input_size + hidden_size, out_features=hidden_size)
+        self.i2o = nn.Linear(in_features=input_size + hidden_size, out_features=output_size)
         self.softmax = nn.LogSoftmax()
 
     def forward(self, input, hidden):
+        print(f"input {input.size()}")
+        print(f"hidden {hidden.size()}")
+
         combined = torch.cat((input, hidden), 1)
+        print(f"Combined {combined.size()}")
+
         hidden = self.i2h(combined)
         output = self.i2o(combined)
         output = self.softmax(output)
         return output, hidden
 
     def initHidden(self):
-        return Variable(torch.zeros(1, self.hidden_size))
+        return Variable(torch.zeros(1, 70)) #return Variable(torch.zeros(1, self.hidden_size))
