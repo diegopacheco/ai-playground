39f28b5fc1003e03d8c0e4ad916b51cb6193f438
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-02 00:26:15 -0700

    update script to install tools

--- install-ai-dev-tools.sh
+++ install-ai-dev-tools.sh
@@ -1,6 +1,11 @@
 #!/bin/bash  
 
+brew install python
+brew install jupyter
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
 pip install -U scikit-learn
 pip install pyqt5
 pip install opencv_python
+pip install numpy
