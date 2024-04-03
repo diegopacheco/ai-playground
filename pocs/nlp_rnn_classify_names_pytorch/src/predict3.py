2432d4512e1a1f984ed62a5515f37bae992404b0
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-03 01:11:58 -0700

    pytorn - RNN / Predict country of name - new predict code

--- pocs/nlp_rnn_classify_names_pytorch/src/predict3.py
+++ pocs/nlp_rnn_classify_names_pytorch/src/predict3.py
@@ -1,6 +1,27 @@
 import torch
+import torch.nn as nn
 
-rnn_loaded = torch.load('../char-rnn-classification.pt')
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.h2o(hidden)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+rnn_loaded = torch.load('char-rnn-classification.pt')
 
 def predict(input_line, n_predictions=3):
     print('\n> %s' % input_line)
