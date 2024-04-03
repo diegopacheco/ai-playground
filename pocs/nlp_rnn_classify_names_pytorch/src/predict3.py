e9e085c3a93d8e2ec4ccf24baafec53d54606f28
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-03 01:09:36 -0700

    pytorn - RNN / Predict country of name - new predict code

---
+++ pocs/nlp_rnn_classify_names_pytorch/src/predict3.py
@@ -0,0 +1,25 @@
+import torch
+
+rnn_loaded = torch.load('../char-rnn-classification.pt')
+
+def predict(input_line, n_predictions=3):
+    print('\n> %s' % input_line)
+    with torch.no_grad():
+        output = evaluate(lineToTensor(input_line))
+
+        # Get top N categories
+        topv, topi = output.topk(n_predictions, 1, True)
+        predictions = []
+
+        for i in range(n_predictions):
+            value = topv[0][i].item()
+            category_index = topi[0][i].item()
+            print('(%.2f) %s' % (value, all_categories[category_index]))
+            predictions.append([value, all_categories[category_index]])
+
+predict('Dovesky')
+predict('Jackson')
+predict('Satoshi')
+predict('Silva')
+predict('Salvatore')
+predict('Pacheco')
