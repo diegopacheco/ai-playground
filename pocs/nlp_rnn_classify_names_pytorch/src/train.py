aae2555dd864b133ccd588da16ed76ea020eaac7
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-03 01:40:03 -0700

    pytorn - RNN / Predict country of name - refactor to improve the code

--- pocs/nlp_rnn_classify_names_pytorch/src/train.py
+++ pocs/nlp_rnn_classify_names_pytorch/src/train.py
@@ -1,73 +1,7 @@
 import torch
 
-from io import open
-import unicodedata
-import string
-import glob
-import os
-import random
-import time
-import math
-
+from data_preparation import *
 from model import *
-
-def findFiles(path): return glob.glob(path)
-
-print(findFiles('data/names/*.txt'))
-
-all_letters = string.ascii_letters + " .,;'"
-n_letters = len(all_letters)
-
-# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
-def unicodeToAscii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-        and c in all_letters
-    )
-
-# Build the category_lines dictionary, a list of names per language
-category_lines = {}
-all_categories = []
-
-# Read a file and split into lines
-def readLines(filename):
-    lines = open(filename, encoding='utf-8').read().strip().split('\n')
-    return [unicodeToAscii(line) for line in lines]
-
-for filename in findFiles('data/names/*.txt'):
-    category = os.path.splitext(os.path.basename(filename))[0]
-    all_categories.append(category)
-    lines = readLines(filename)
-    category_lines[category] = lines
-
-n_categories = len(all_categories)
-
-#
-# Names into tensor
-# 
-
-# Find letter index from all_letters, e.g. "a" = 0
-def letterToIndex(letter):
-    return all_letters.find(letter)
-
-# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
-def letterToTensor(letter):
-    tensor = torch.zeros(1, n_letters)
-    tensor[0][letterToIndex(letter)] = 1
-    return tensor
-
-# Turn a line into a <line_length x 1 x n_letters>,
-# or an array of one-hot letter vectors
-def lineToTensor(line):
-    tensor = torch.zeros(len(line), 1, n_letters)
-    for li, letter in enumerate(line):
-        tensor[li][0][letterToIndex(letter)] = 1
-    return tensor
-
-print(letterToTensor('J'))
-
-print(lineToTensor('Jones').size())
 
 #
 # Neural Network
@@ -81,7 +15,6 @@ def categoryFromOutput(output):
     top_n, top_i = output.topk(1)
     category_i = top_i[0].item()
     return all_categories[category_i], category_i
-
 
 def randomChoice(l):
     return l[random.randint(0, len(l) - 1)]
@@ -98,7 +31,6 @@ for i in range(10):
     print('category =', category, '/ line =', line)
 
 criterion = nn.NLLLoss()
-
 learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
 
 def train(category_tensor, line_tensor):
