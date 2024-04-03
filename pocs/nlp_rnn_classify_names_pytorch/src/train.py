e76d8da48e6b73f62fb29c5e623dc6f33077d027
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-03 01:30:51 -0700

    pytorn - RNN / Predict country of name - refactor to improve the code

--- pocs/nlp_rnn_classify_names_pytorch/src/train.py
+++ pocs/nlp_rnn_classify_names_pytorch/src/train.py
@@ -1,4 +1,5 @@
 import torch
+from model import *
 
 #
 # Data preparation
@@ -75,30 +76,6 @@ print(lineToTensor('Jones').size())
 #
 # Neural Network
 #
-
-import torch.nn as nn
-
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
-
-        self.hidden_size = hidden_size
-
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.h2o = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        combined = torch.cat((input, hidden), 1)
-        hidden = self.i2h(combined)
-        output = self.h2o(hidden)
-        output = self.softmax(output)
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, self.hidden_size)
-
-n_hidden = 128
 rnn = RNN(n_letters, n_hidden, n_categories)
 
 #
