e3a7f7c08d92c9984f1e623d64185ce23cf94b9b
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-03 01:45:30 -0700

    pytorn - RNN / Predict country of name - refactor to improve the code

--- pocs/nlp_rnn_classify_names_pytorch/src/predict.py
+++ pocs/nlp_rnn_classify_names_pytorch/src/predict.py
@@ -1,44 +1,8 @@
 import torch
 import torch.nn as nn
 
-import unicodedata
-import string
-from io import open
-import glob
-import os
-import random
-
 from model import *
-
-def findFiles(path): return glob.glob(path)
-
-all_letters = string.ascii_letters + " .,;'"
-n_letters = len(all_letters)
-
-# Build the category_lines dictionary, a list of names per language
-category_lines = {}
-all_categories = []
-
-# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
-def unicodeToAscii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-        and c in all_letters
-    )
-
-# Read a file and split into lines
-def readLines(filename):
-    lines = open(filename, encoding='utf-8').read().strip().split('\n')
-    return [unicodeToAscii(line) for line in lines]
-
-for filename in findFiles('data/names/*.txt'):
-    category = os.path.splitext(os.path.basename(filename))[0]
-    all_categories.append(category)
-    lines = readLines(filename)
-    category_lines[category] = lines
-
-n_categories = len(all_categories)
+from data_preparation import *
 
 n_hidden = 128
 rnn = RNN(n_letters, n_hidden, n_categories)
@@ -61,7 +25,6 @@ def unicodeToAscii(s):
         if unicodedata.category(c) != 'Mn'
         and c in all_letters
     )
-
 
 # Find letter index from all_letters, e.g. "a" = 0
 def letterToIndex(letter):
