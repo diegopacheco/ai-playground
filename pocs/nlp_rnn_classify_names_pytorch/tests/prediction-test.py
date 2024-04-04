a3d2a14c8980975619e74b5723d9e4312b353a49
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-04 02:11:39 -0700

    pytorn - RNN / Predict country of name - adding tests

--- pocs/nlp_rnn_classify_names_pytorch/tests/prediction-test.py
+++ pocs/nlp_rnn_classify_names_pytorch/tests/prediction-test.py
@@ -2,13 +2,16 @@ import unittest
 import os, sys
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+sys.path.append('src/')
 from src import predict
 
 class TestStringMethods(unittest.TestCase):
 
     def test_portuguese_prediction_present(self):
-        result = predict.predict("Pacheco")
+        result = predict.predict("Silva")
         print(result)
+        if not 'Portuguese' in result:
+            self.fail("Should be a portuguese name")
 
 if __name__ == '__main__':
     unittest.main()
