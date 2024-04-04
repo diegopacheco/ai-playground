a330cb523faaebb7abf293a8b8e9ec36befc82e2
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-04 01:57:59 -0700

    pytorn - RNN / Predict country of name - adding tests

--- pocs/nlp_rnn_classify_names_pytorch/tests/check-data-test.py
+++ pocs/nlp_rnn_classify_names_pytorch/tests/check-data-test.py
@@ -1,20 +1,28 @@
 import unittest
+from pathlib import Path
+
+def check_file(self,f):
+        my_file = Path(f)
+        if not my_file.is_file():
+            self.fail(f"{my_file} does not exist")
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_data_is_present(self):
+        check_file(self,"data/names/Portuguese.txt")
+        check_file(self,"data/names/Arabic.txt")
+        check_file(self,"data/names/Czech.txt")
+        check_file(self,"data/names/Dutch.txt")
+        check_file(self,"data/names/French.txt")
+        check_file(self,"data/names/German.txt")
+        check_file(self,"data/names/Greek.txt")
+        check_file(self,"data/names/Irish.txt")
+        check_file(self,"data/names/Italian.txt")
+        check_file(self,"data/names/Japanese.txt")
+        check_file(self,"data/names/Korean.txt")
+        check_file(self,"data/names/Scottish.txt")
+        check_file(self,"data/names/Spanish.txt")
+        check_file(self,"data/names/Vietnamese.txt")
 
 if __name__ == '__main__':
     unittest.main()
