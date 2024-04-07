8edf08a34527a5d24e083e8335740d466e64f23d
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-07 00:19:29 -0700

    pytorch - double deep q networks - mario rl agent

--- pocs/pytorch-double-deep-q-networks-mario-rl-agent/src/main.py
+++ pocs/pytorch-double-deep-q-networks-mario-rl-agent/src/main.py
@@ -440,3 +440,49 @@ class MetricLogger:
             plt.legend()
             plt.savefig(getattr(self, f"{metric}_plot"))
 
+use_cuda = torch.cuda.is_available()
+print(f"Using CUDA: {use_cuda}")
+print()
+
+save_dir = Path("checkpoints") / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+save_dir.mkdir(parents=True)
+
+mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir)
+
+logger = MetricLogger(save_dir)
+
+episodes = 40
+for e in range(episodes):
+
+    state = env.reset()
+
+    # Play the game!
+    while True:
+
+        # Run agent on the state
+        action = mario.act(state)
+
+        # Agent performs action
+        next_state, reward, done, trunc, info = env.step(action)
+
+        # Remember
+        mario.cache(state, next_state, action, reward, done)
+
+        # Learn
+        q, loss = mario.learn()
+
+        # Logging
+        logger.log_step(reward, loss, q)
+
+        # Update state
+        state = next_state
+
+        # Check if end of game
+        if done or info["flag_get"]:
+            break
+
+    logger.log_episode()
+
+    if (e % 20 == 0) or (e == episodes - 1):
+        logger.record(episode=e, epsilon=mario.exploration_rate, step=mario.curr_step)
+
