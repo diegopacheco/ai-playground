beab13c829a80db35c11fcdd83d05686ad7a12bb
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-07 00:18:21 -0700

    pytorch - double deep q networks - mario rl agent

--- pocs/pytorch-double-deep-q-networks-mario-rl-agent/src/main.py
+++ pocs/pytorch-double-deep-q-networks-mario-rl-agent/src/main.py
@@ -299,3 +299,37 @@ class Mario(Mario):
         )
         print(f"MarioNet saved to {save_path} at step {self.curr_step}")
 
+class Mario(Mario):
+    def __init__(self, state_dim, action_dim, save_dir):
+        super().__init__(state_dim, action_dim, save_dir)
+        self.burnin = 1e4  # min. experiences before training
+        self.learn_every = 3  # no. of experiences between updates to Q_online
+        self.sync_every = 1e4  # no. of experiences between Q_target & Q_online sync
+
+    def learn(self):
+        if self.curr_step % self.sync_every == 0:
+            self.sync_Q_target()
+
+        if self.curr_step % self.save_every == 0:
+            self.save()
+
+        if self.curr_step < self.burnin:
+            return None, None
+
+        if self.curr_step % self.learn_every != 0:
+            return None, None
+
+        # Sample from memory
+        state, next_state, action, reward, done = self.recall()
+
+        # Get TD Estimate
+        td_est = self.td_estimate(state, action)
+
+        # Get TD Target
+        td_tgt = self.td_target(reward, next_state, done)
+
+        # Backpropagate loss through Q_online
+        loss = self.update_Q_online(td_est, td_tgt)
+
+        return (td_est.mean().item(), loss)
+    
