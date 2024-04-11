54578bddf0b650eb29dcbc29c09463a73a8bb248
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-11 02:30:34 -0700

    pandas ml - titanic dataset

--- pocs/pandas-ml-titanic/src/main.py
+++ pocs/pandas-ml-titanic/src/main.py
@@ -1,1 +1,20 @@
-print('Hello, world!')
+import numpy as np
+import pandas as pd
+
+# Set seed for reproducibility
+np.random.seed(seed=1234)
+
+df = pd.read_csv('data/train.csv', header=0)
+print(df.head(3))
+
+import matplotlib.pyplot as plt
+# Describe features
+print(df.describe())
+
+# Correlation matrix
+plt.matshow(df.corr())
+continuous_features = df.describe(exclude='Name').columns
+plt.xticks(range(len(continuous_features)), continuous_features, rotation="45")
+plt.yticks(range(len(continuous_features)), continuous_features, rotation="45")
+plt.colorbar()
+plt.show()
