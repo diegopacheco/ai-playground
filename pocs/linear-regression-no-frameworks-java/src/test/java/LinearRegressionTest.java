a4c3d05facd69ac832d3db3d1dee5b03cf76dfcb
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-15 00:13:31 -0700

    lenear regression java

--- pocs/linear-regression-no-frameworks-java/src/test/java/LinearRegressionTest.java
+++ pocs/linear-regression-no-frameworks-java/src/test/java/LinearRegressionTest.java
@@ -25,5 +25,32 @@ public class LinearRegressionTest {
         double[] y = {2, 3, 4, 5, 6};
         LinearRegression lr = new LinearRegression(x, y);
         assertEquals(7, lr.predict(6), 0.0001);
+        assertEquals(11, lr.predict(10), 0.0001);
+        assertEquals(100, lr.predict(99), 0.0001);
+    }
+
+
+    @Test
+    public void testR2() {
+        double[] x = {1, 2, 3, 4, 5};
+        double[] y = {2, 3, 4, 5, 6};
+        LinearRegression lr = new LinearRegression(x, y);
+        assertEquals(1, lr.r2(), 0.0001);
+    }
+
+    @Test
+    public void testInterceptStdErr() {
+        double[] x = {1, 2, 3, 4, 5};
+        double[] y = {2, 3, 4, 5, 6};
+        LinearRegression lr = new LinearRegression(x, y);
+        assertEquals(0, lr.interceptStdErr(), 0.0001);
+    }
+
+    @Test
+    public void testSlopeStdErr() {
+        double[] x = {1, 2, 3, 4, 5};
+        double[] y = {2, 3, 4, 5, 6};
+        LinearRegression lr = new LinearRegression(x, y);
+        assertEquals(0, lr.slopeStdErr(), 0.0001);
     }
 }
