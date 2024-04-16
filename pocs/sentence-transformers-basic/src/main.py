7d81ae10475056ef61551493bb978808108b07f0
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-16 01:25:01 -0700

    refactor the code

--- pocs/sentence-transformers-basic/src/main.py
+++ pocs/sentence-transformers-basic/src/main.py
@@ -1,18 +1,20 @@
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-# Our sentences to encode
-sentences = [
+def sentense_to_embeddings(sentence):
+    # Sentences are encoded by calling model.encode()
+    embeddings = model.encode(sentence)
+
+    # Print the embeddings
+    for sentence, embedding in zip(sentence, embeddings):
+        print("Sentence:", sentence)
+        print("Embedding:", embedding)
+        print("")    
+
+    return embeddings
+
+sentense_to_embeddings(sentence = [
     "This framework generates embeddings for each input sentence",
     "Sentences are passed as a list of string.",
     "The quick brown fox jumps over the lazy dog."
-]
-
-# Sentences are encoded by calling model.encode()
-embeddings = model.encode(sentences)
-
-# Print the embeddings
-for sentence, embedding in zip(sentences, embeddings):
-    print("Sentence:", sentence)
-    print("Embedding:", embedding)
-    print("")
+])
