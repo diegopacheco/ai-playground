a26ad5e159d23f411fe1d3a278775d522a01ae4a
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-17 01:35:56 -0700

    stable diffusion with diffusers

--- pocs/stable-diffusion/src/main.py
+++ pocs/stable-diffusion/src/main.py
@@ -4,10 +4,10 @@ from diffusers import StableDiffusionPipeline
 model_id = "CompVis/stable-diffusion-v1-4"
 device = "cpu"
 
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 pipe = pipe.to(device)
 
-prompt = "a photo of an astronaut riding a white cat on mars"
+prompt = "a photo of an black astronaut riding a pig on mars"
 image = pipe(prompt).images[0]  
     
 image.save("astronaut.png")
