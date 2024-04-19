4b9a3656b686a4273c137719eff1b175c757c865
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-19 03:50:57 -0700

    llma3 simple fun

--- pocs/llama-3-fun/src/main.py
+++ pocs/llama-3-fun/src/main.py
@@ -1,13 +1,16 @@
 import transformers
+from transformers import AutoTokenizer
 import torch
 
 model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+tokenizer = AutoTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct')
 
 pipeline = transformers.pipeline(
     "text-generation",
     model=model_id,
     model_kwargs={"torch_dtype": torch.bfloat16},
-    device="cuda",
+    device="cpu",
 )
 
 messages = [
