9c644bbb26a3017684b09908e84d2caa5d1a8ed6
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-20 02:05:37 -0700

    transformers-YOLO for image detection

---
+++ pocs/transformers-YOLOs/src/main.py
@@ -0,0 +1,26 @@
+from transformers import AutoImageProcessor, AutoModelForObjectDetection
+import torch
+from PIL import Image
+import requests
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+image_processor = AutoImageProcessor.from_pretrained("hustvl/yolos-tiny")
+model = AutoModelForObjectDetection.from_pretrained("hustvl/yolos-tiny")
+
+inputs = image_processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+
+# convert outputs (bounding boxes and class logits) to Pascal VOC format (xmin, ymin, xmax, ymax)
+target_sizes = torch.tensor([image.size[::-1]])
+results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[
+    0
+]
+
+for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+    box = [round(i, 2) for i in box.tolist()]
+    print(
+        f"Detected {model.config.id2label[label.item()]} with confidence "
+        f"{round(score.item(), 3)} at location {box}"
+    )
