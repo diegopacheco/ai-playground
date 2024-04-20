3b40b7300eed635af3f800ef23c40c9a1dfae7b6
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-20 02:30:39 -0700

    transformers-GPTj

---
+++ pocs/transformers-GPTj/src/main.py
@@ -0,0 +1,25 @@
+from transformers import GPTJForCausalLM, AutoTokenizer
+import torch
+
+device = "cpu"
+model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", torch_dtype=torch.float16).to(device)
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+
+prompt = (
+    "Please give me the output in JSON format."
+    "In a shocking finding, scientists discovered a herd of unicorns living in a remote, "
+    "previously unexplored valley, in the Andes Mountains. Even more surprising to the "
+    "researchers was the fact that the unicorns spoke perfect English."
+    "Is this a joke? Who wrote this? I'm not sure what to make of this."
+)
+
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+
+gen_tokens = model.generate(
+    input_ids,
+    do_sample=True,
+    temperature=0.9,
+    max_length=100,
+)
+gen_text = tokenizer.batch_decode(gen_tokens)[0]
+print(gen_text)
