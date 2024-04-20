7c36d267a93c035473b8d3fb6f8b58b74d124254
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-20 02:50:01 -0700

    transformers-bark

--- pocs/transformers-bark/src/main.py
+++ pocs/transformers-bark/src/main.py
@@ -6,7 +6,7 @@ model = BarkModel.from_pretrained("suno/bark")
 
 voice_preset = "v2/en_speaker_6"
 
-inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
+inputs = processor("Brazil! It's not for amateurs", voice_preset=voice_preset)
 
 audio_array = model.generate(**inputs)
 audio_array = audio_array.cpu().numpy().squeeze()
