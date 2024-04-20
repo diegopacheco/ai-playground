fadaafdd607e51dbf04aca770f65a34440484d73
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-20 02:39:41 -0700

    transformers-bark

---
+++ pocs/transformers-bark/src/main.py
@@ -0,0 +1,16 @@
+from transformers import AutoProcessor, BarkModel
+import soundfile as sf
+
+processor = AutoProcessor.from_pretrained("suno/bark")
+model = BarkModel.from_pretrained("suno/bark")
+
+voice_preset = "v2/en_speaker_6"
+
+inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
+
+audio_array = model.generate(**inputs)
+audio_array = audio_array.cpu().numpy().squeeze()
+
+# save the audio
+sf.write("output.wav", audio_array, 22050)
+print("Audio saved to output.wav")
