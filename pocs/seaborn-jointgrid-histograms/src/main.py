53f1879ee4ff6770c250a5830c6f760daa793d30
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-21 00:27:33 -0700

    seaborn visualization - jointgrid histograns

---
+++ pocs/seaborn-jointgrid-histograms/src/main.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_theme(style="white")
+
+df = sns.load_dataset("penguins")
+
+g = sns.JointGrid(data=df, x="body_mass_g", y="bill_depth_mm", space=0)
+g.plot_joint(sns.kdeplot,
+             fill=True, clip=((2200, 6800), (10, 25)),
+             thresh=0, levels=100, cmap="rocket")
+g.plot_marginals(sns.histplot, color="#03051A", alpha=1, bins=25)
+
+# plot 
+plt.show()
