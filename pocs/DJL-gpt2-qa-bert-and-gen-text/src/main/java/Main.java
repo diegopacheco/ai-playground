3e6e80d2c05bca2cd4469fd1fea64ed16bf02327
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-23 19:45:20 -0700

    better names, fix the code to work on console

---
+++ pocs/DJL-gpt2-qa-bert-and-gen-text/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        GPT2GenerateText.main(null);
+        BertQaInference.main(null);
+
+        System.out.printf("DONE.");
+    }
+}
