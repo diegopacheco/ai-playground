1469e8cd5ff59d33551d08af835b7544982260b7
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-26 00:02:13 -0700

    transformers - image caption

--- pocs/transformers-image-caption/src/main.py
+++ pocs/transformers-image-caption/src/main.py
@@ -97,7 +97,3 @@ print(image)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 inputs = processor(images=image, return_tensors="pt").to(device)
 pixel_values = inputs.pixel_values
-
-generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(generated_caption)
