285d7b004505169927d7b1caf2757f5cc71144c0
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-27 02:24:11 -0700

    transformers-diffusers image to image

--- pocs/diffusers-image-to-image/src/main.py
+++ pocs/diffusers-image-to-image/src/main.py
@@ -2,13 +2,18 @@ import torch
 from diffusers import AutoPipelineForImage2Image
 from diffusers.utils import load_image, make_image_grid
 
+device = torch.device('cpu')
+
 pipeline = AutoPipelineForImage2Image.from_pretrained(
-    "kandinsky-community/kandinsky-2-2-decoder", torch_dtype=torch.float16, use_safetensors=True
+    "kandinsky-community/kandinsky-2-2-decoder", 
+    torch_dtype=torch.float16, 
+    use_safetensors=True,
+    device=device
 )
 pipeline.enable_model_cpu_offload()
 
 # remove following line if xFormers is not installed or you have PyTorch 2.0 or higher installed
-pipeline.enable_xformers_memory_efficient_attention()
+#pipeline.enable_xformers_memory_efficient_attention()
 
 init_image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png")
 prompt = "cat wizard, gandalf, lord of the rings, detailed, fantasy, cute, adorable, Pixar, Disney, 8k"
