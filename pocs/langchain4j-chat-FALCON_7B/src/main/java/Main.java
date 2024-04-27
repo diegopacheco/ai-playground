f3692e0a5942cd245266b62a9e7d7df0c6abab1f
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-27 01:26:33 -0700

    langchain4j - chat - all-MiniLM-L6-v2

--- pocs/langchain4j-chat-FALCON_7B/src/main/java/Main.java
+++ pocs/langchain4j-chat-FALCON_7B/src/main/java/Main.java
@@ -1,24 +1,26 @@
-import dev.langchain4j.model.huggingface.HuggingFaceLanguageModel;
-
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.model.huggingface.HuggingFaceChatModel;
+import static dev.langchain4j.data.message.SystemMessage.systemMessage;
+import static dev.langchain4j.data.message.UserMessage.userMessage;
 import static dev.langchain4j.model.huggingface.HuggingFaceModelName.TII_UAE_FALCON_7B_INSTRUCT;
 import static java.time.Duration.ofSeconds;
 
 public class Main{
   public static void main(String args[]){
-    HuggingFaceLanguageModel model = HuggingFaceLanguageModel.builder()
+    HuggingFaceChatModel model = HuggingFaceChatModel.builder()
             .accessToken(System.getenv("HF_TOKEN"))
             .modelId(TII_UAE_FALCON_7B_INSTRUCT)
-            .timeout(ofSeconds(15))
+            .timeout(ofSeconds(30))
             .temperature(0.7)
             .maxNewTokens(20)
             .waitForModel(true)
             .build();
 
-    String answer = model.generate("What is the capital of the USA?").content();
-    System.out.println(answer);
+    AiMessage aiMessage = model.generate(
+            systemMessage("You are a good friend of mine, who likes to answer with jokes"),
+            userMessage("Hey Bro, what are you doing?")
+    ).content();
 
-    System.out.println(model.generate("What is the capital of Brazil?").content());
-
-    System.out.println(model.generate("For what team Steph Curry plays?").content());
+    System.out.println(aiMessage.text());
   }
 }
