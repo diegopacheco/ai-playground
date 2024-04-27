a420f4d9aa132fc34e5bd520da33c0e8b2ed7f7c
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-27 01:12:58 -0700

    langchain4j - embeddings - all-MiniLM-L6-v2

---
+++ pocs/langchain4j-embeddings-all-MiniLM-L6-v2/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import dev.langchain4j.model.huggingface.HuggingFaceLanguageModel;
+
+import static dev.langchain4j.model.huggingface.HuggingFaceModelName.TII_UAE_FALCON_7B_INSTRUCT;
+import static java.time.Duration.ofSeconds;
+
+public class Main{
+  public static void main(String args[]){
+    HuggingFaceLanguageModel model = HuggingFaceLanguageModel.builder()
+            .accessToken(System.getenv("HF_TOKEN"))
+            .modelId(TII_UAE_FALCON_7B_INSTRUCT)
+            .timeout(ofSeconds(15))
+            .temperature(0.7)
+            .maxNewTokens(20)
+            .waitForModel(true)
+            .build();
+
+    String answer = model.generate("What is the capital of the USA?").content();
+    System.out.println(answer);
+
+    System.out.println(model.generate("What is the capital of Brazil?").content());
+
+    System.out.println(model.generate("For what team Steph Curry plays?").content());
+  }
+}
