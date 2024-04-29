e75e15382c07038308900f400481276800f7d6f1
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-29 00:22:00 -0700

    langchain4j - document embeddings - LocalAI

---
+++ pocs/langchain4j-local-ai-document-embeddings/src/main/java/Main.java
@@ -0,0 +1,62 @@
+import dev.langchain4j.chain.ConversationalRetrievalChain;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.localai.LocalAiChatModel;
+import dev.langchain4j.retriever.EmbeddingStoreRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+
+import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.loadDocument;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+                .documentSplitter(DocumentSplitters.recursive(500, 0))
+                .embeddingModel(embeddingModel)
+                .embeddingStore(embeddingStore)
+                .build();
+
+        //String basePath = new File(".").getCanonicalPath();
+        Document johanCruiffInfo = loadDocument(toPath("Johan_Cruyff.pdf"));
+        ingestor.ingest(johanCruiffInfo);
+
+        ChatLanguageModel model = LocalAiChatModel.builder()
+                .baseUrl("http://localhost:8080")
+                .modelName("gpt-4")
+                .temperature(0.0)
+                .timeout(Duration.ofMinutes(5))
+                .build();
+
+        ConversationalRetrievalChain chain = ConversationalRetrievalChain.builder()
+                .chatLanguageModel(model)
+                .retriever(EmbeddingStoreRetriever.from(embeddingStore, embeddingModel))
+                .build();
+
+        String answer = chain.execute("Give all football teams Johan Cruijff played for in his senior career");
+        System.out.println(answer);
+    }
+
+    private static Path toPath(String fileName) {
+        try {
+            URL fileUrl = Main.class.getClassLoader().getResource(fileName);
+            return Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
