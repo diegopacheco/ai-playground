6b8e561fcf4db28119aface2f7baa207594c0536
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-30 01:00:07 -0700

    improved localai script

--- run-local-ai.sh
+++ run-local-ai.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-podman run --rm -p 8080:8080 --name local-ai -ti localai/localai:latest-aio-cpu
-
+podman run -it --rm -v ~/localai/:/build/models:cached -p 8080:8080 --name local-ai -ti localai/localai:latest-aio-cpu
