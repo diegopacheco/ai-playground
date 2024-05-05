3c47555d29fb1b29ce0f9a3a1d624059c25b64ce
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-05 02:56:29 -0700

    text to video generation

--- pocs/diffusers-text-to-video/src/main.py
+++ pocs/diffusers-text-to-video/src/main.py
@@ -7,13 +7,19 @@ import gradio as gr
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 mdevice = torch.device('cpu')
-pipe = DiffusionPipeline.from_pretrained(device=mdevice,pretrained_model_name_or_path="damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float32, variant="fp16")
-pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config,device=mdevice)
-#pipe.enable_model_cpu_offload()
+pipe = DiffusionPipeline.from_pretrained(device=mdevice, pretrained_model_name_or_path="damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float32, variant="fp16")
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config, device=mdevice)
 
 def text_to_video(prompt):
     video_frames = pipe(prompt, num_inference_steps=25).frames
-    video_path = export_to_video(video_frames)
+    # Ensure video_frames[0] is a 3D tensor
+    if len(video_frames[0].shape) == 4:
+        # Select the first frame from the batch
+        frame = video_frames[0][0]
+    else:
+        frame = video_frames[0]
+
+    video_path = export_to_video([frame])
     return video_path
 
 gr.Interface(fn=text_to_video, inputs="text", outputs="file").launch()
