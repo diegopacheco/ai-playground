cda16d8bd303cfe5fa77892b6e6451fca28564c2
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-06 00:59:35 -0700

    comics

--- pocs/transfomers-unconditional-image-generation/src/main.py
+++ pocs/transfomers-unconditional-image-generation/src/main.py
@@ -2,6 +2,7 @@ from diffusers import DiffusionPipeline
 from torchvision.transforms.functional import to_tensor
 import torch
 import matplotlib.pyplot as plt
+from torchvision.transforms.functional import to_pil_image
 import gradio as gr
 
 def text_to_comics(text):
@@ -11,18 +12,18 @@ def text_to_comics(text):
     image = output.images[0]
     image_tensor = to_tensor(image)
     image_tensor = image_tensor.permute(1, 2, 0)
-    plt.imshow(image_tensor.cpu().numpy())
-    plt.show()
-
-    torch.save(image_tensor, "comics.png")
+    image_pil = to_pil_image(image_tensor)
+    image_pil.save("comics.png")
     print("Image saved as comics.png")
+    return image_pil
 
-text_to_comics("turtles fithing epic mosquitos in the swear of the city")
+e1 = text_to_comics("turtles fithing epic mosquitos in the swear of the city")
 
 # ui with gradio
 ui = gr.Interface(fn=text_to_comics, 
                   inputs="text", 
                   outputs="image",
-                  title="Type some text get comics!"
+                  title="Type some text get comics!",
+                  examples=[("turtles fithing epic mosquitos in the swear of the city", e1)],
                  )
 ui.launch()
