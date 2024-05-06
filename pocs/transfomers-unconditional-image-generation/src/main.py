d4179ffb3d981d3b11e9975b08657d4da98ae9fc
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-06 03:53:45 -0700

    ai poc

--- pocs/transfomers-unconditional-image-generation/src/main.py
+++ pocs/transfomers-unconditional-image-generation/src/main.py
@@ -6,6 +6,12 @@ from torchvision.transforms.functional import to_pil_image
 import gradio as gr
 from torchvision.utils import save_image
 from PIL import Image
+import os
+from gtts import gTTS
+import torch
+import gradio as gr
+from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+from transformers import pipeline, GPT2LMHeadModel, GPT2Tokenizer
 
 example_1 = "ninja turtles fithing against a mosquito, in the sea"
 example_2 = "warrior fithing zombies with a sword, in the forest"
@@ -43,13 +49,51 @@ def text_to_comics(text):
 #text_to_comics(example_2)
 #text_to_comics(example_3)
 
-ui = gr.Interface(fn=text_to_comics, 
+def generate_story(description):
+    model = GPT2LMHeadModel.from_pretrained("gpt2")
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    inputs = tokenizer.encode(description + " a thriller/action story.", return_tensors='pt')
+    outputs = model.generate(input_ids=inputs, 
+                             max_length=200, 
+                             num_return_sequences=1, 
+                             temperature=0.7, 
+                             no_repeat_ngram_size=2)
+    story = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return story
+
+def convert_to_audio(text):
+    tts = gTTS(text)
+    audio_file_path = "audio.mp3"
+    tts.save(audio_file_path)
+    return audio_file_path
+
+def audio_to_text(audio_file_path):
+    pipe = pipeline("automatic-speech-recognition", "openai/whisper-large-v2")
+    result = pipe("audio.mp3")
+    print(result)
+    return result['text']
+
+def sentiment_analysis(text):
+    sentiment_analyzer = pipeline("sentiment-analysis")
+    result = sentiment_analyzer(text)
+    print(result)
+    return result
+
+def app(text):
+    comics = text_to_comics(text)
+    story = generate_story(text)
+    audio_file = convert_to_audio(story)
+    transcribed_text = audio_to_text(audio_file)
+    sentiment = sentiment_analysis(transcribed_text)
+    return comics, audio_file,transcribed_text, sentiment
+
+ui = gr.Interface(fn=app, 
                   inputs="text", 
-                  outputs=["image" for _ in range(1)],
-                  title="GenAI LLM comics: Type some text get comics!",
+                  outputs=["image", "audio", "text", "text"],
+                  title="GenAI Multi-model LLM comics: Type some text get comics!",
                   description="This model generates comics based on the text(max 70 chars) you provide." + \
                     "<BR/>It does not work on mobile(timeout issue) click on examples if dont want to wait. " + \
-                    "<BR/> It may take ~10-20min to generate the comics.",
+                    "<BR/>It may take ~10-20min to generate the comics.",
                   examples=[(example_1),(example_2),(example_3)],
                  )
 ui.launch()
