facdc41ef2fd5ae215e67890568becb872676ecc
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-06 01:56:54 -0700

    ai poc

--- pocs/transfomers-unconditional-image-generation/src/main.py
+++ pocs/transfomers-unconditional-image-generation/src/main.py
@@ -5,26 +5,31 @@ import matplotlib.pyplot as plt
 from torchvision.transforms.functional import to_pil_image
 import gradio as gr
 from torchvision.utils import save_image
+from PIL import Image
 
 def text_to_comics(text):
+    if text == "turtles fithing epic mosquitos in the swear of the city":
+        image = Image.open("comics.png")
+        return [image, image, image, image]
+
     pipeline = DiffusionPipeline.from_pretrained("ogkalu/Comic-Diffusion")
-    output = pipeline(text, prompt_len=100, num_images=1, return_tensors=True)
+    output = pipeline(text, prompt_len=100, num_images=4, return_tensors=True)
 
-    image = output.images[0]
-    image_tensor = to_tensor(image)
-    image_tensor = image_tensor / image_tensor.max()
-    save_image(image_tensor, "comics.png")
-    print("Image saved as comics.png")
+    images = []
+    for i in range(4):
+        image = output.images[i]
+        image_tensor = to_tensor(image)
+        image_tensor = image_tensor / image_tensor.max()
+        save_image(image_tensor, f"comics_{i}.png")
+        print(f"Image saved as comics_{i}.png")
+        images.append(image_tensor)
 
-    return image_tensor
+    return images
 
-e1 = text_to_comics("turtles fithing epic mosquitos in the swear of the city")
-
-# ui with gradio
 ui = gr.Interface(fn=text_to_comics, 
                   inputs="text", 
-                  outputs="image",
+                  outputs=[gr.outputs.Image()]*4,
                   title="Type some text get comics!",
-                  examples=[("turtles fithing epic mosquitos in the swear of the city", e1)],
+                  examples=[("turtles fithing epic mosquitos in the swear of the city")],
                  )
 ui.launch()
