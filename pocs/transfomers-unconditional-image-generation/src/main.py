763c8e0a6364632ab2644e0f37bd6dfa5f7fd226
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-06 01:10:25 -0700

    comics

--- pocs/transfomers-unconditional-image-generation/src/main.py
+++ pocs/transfomers-unconditional-image-generation/src/main.py
@@ -8,11 +8,18 @@ import gradio as gr
 def text_to_comics(text):
     pipeline = DiffusionPipeline.from_pretrained("ogkalu/Comic-Diffusion")
     output = pipeline(text, prompt_len=100, num_images=1, return_tensors=True)
-
     image = output.images[0]
     image_tensor = to_tensor(image)
-    image_tensor = image_tensor.permute(1, 2, 0)
-    image_pil = to_pil_image(image_tensor)
+
+    # Check the shape of the tensor before permuting
+    if image_tensor.shape[0] > 4:
+        # The tensor already has the shape (H, W, C), so no need to permute
+        image_pil = to_pil_image(image_tensor)
+    else:
+        # The tensor has the shape (C, H, W), so permute the dimensions
+        image_tensor = image_tensor.permute(1, 2, 0)
+        image_pil = to_pil_image(image_tensor)
+
     image_pil.save("comics.png")
     print("Image saved as comics.png")
     return image_pil
