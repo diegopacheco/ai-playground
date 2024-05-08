11aff5c83767aa47cf7fdcee328499d3061c2769
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-08 00:43:31 -0700

    reinforcement learning car

---
+++ pocs/reinforcement-learning-CarRacing/src/main.py
@@ -0,0 +1,25 @@
+import gym
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+env = gym.make("CarRacing-v2", domain_randomize=True,render_mode="rgb_array")
+observation, info = env.reset(seed=42)
+recorder = VideoRecorder(env, path='car.mp4')
+
+try:
+    for i in range(1000):
+        print(f"step {i}")
+        action = env.action_space.sample() # samples random action from action sample space
+        # the agent takes the action
+        observation, reward, terminated, trunc, info = env.step(action)  # Corrected this line
+
+        env.render()
+        recorder.capture_frame()
+        print(f"observation: {observation}")
+
+        # if the agent reaches terminal state, we reset the environment
+        if terminated:
+            print("Environment is reset")
+            observation = env.reset()
+finally:
+    recorder.close()
+    env.close()
