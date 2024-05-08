770ef187d48aa20ccf15761c71e232f8cf088a4f
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-08 00:38:42 -0700

    reinforcement learning lunar

--- pocs/reinforcement-learning-LunarLander/README.md
+++ pocs/reinforcement-learning-LunarLander/README.md
@@ -1,5 +1,8 @@
 ### Result
-* TODO
+* Reinforcement Learning
+* Using Gym
+* Model LunarLander-v2 
 
-<img src='' />
-
+<video width="320" height="240" controls>
+  <source src="lunar.mp4" type="video/mp4">
+</video>
--- pocs/reinforcement-learning-LunarLander/src/main.py
+++ pocs/reinforcement-learning-LunarLander/src/main.py
@@ -1,25 +1,25 @@
 import gym
 from gym.wrappers.monitoring.video_recorder import VideoRecorder
 
-#env = gym.make("CartPole-v1",render_mode="rgb_array")
-env = gym.make("LunarLander-v2", render_mode="human")
+env = gym.make("LunarLander-v2", render_mode="rgb_array")
 observation, info = env.reset(seed=42)
 recorder = VideoRecorder(env, path='lunar.mp4')
 
-for i in range(1000):
-    print(f"step {i}")
-    action = env.action_space.sample() # samples random action from action sample space
-    # the agent takes the action
-    observation, reward, terminated, trunc, info = env.step(action)  # Corrected this line
+try:
+    for i in range(1000):
+        print(f"step {i}")
+        action = env.action_space.sample() # samples random action from action sample space
+        # the agent takes the action
+        observation, reward, terminated, trunc, info = env.step(action)  # Corrected this line
 
-    env.render()
-    recorder.capture_frame()
-    print(f"observation: {observation}")
+        env.render()
+        recorder.capture_frame()
+        print(f"observation: {observation}")
 
-    # if the agent reaches terminal state, we reset the environment
-    if terminated:
-        print("Environment is reset")
-        observation = env.reset()
-
-recorder.close()
-env.close()
+        # if the agent reaches terminal state, we reset the environment
+        if terminated:
+            print("Environment is reset")
+            observation = env.reset()
+finally:
+    recorder.close()
+    env.close()
