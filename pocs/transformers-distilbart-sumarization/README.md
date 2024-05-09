1ade8b73167975f6ec39748d1b4c193318ddf267
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-09 00:22:38 -0700

    distilbart sumarization

---
+++ pocs/transformers-distilbart-sumarization/README.md
@@ -0,0 +1,11 @@
+### Result
+* Sumarization
+
+Original:
+```
+Paris is the capital and most populous city of France, with an estimated population of 2,175,601 residents as of 2018 . The city is the centre and seat of government of the region and province of Île-de-France, or Paris Region . Paris Region has an estimated 18 percent of the population of France as of 2017 .
+```
+Result:
+```
+[{'summary_text': ' Paris is the capital and most populous city of France, with an estimated population of 2,175,601 residents . The City of Paris'}]
+```
---
+++ pocs/transformers-distilbart-sumarization/src/main.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+
+classifier = pipeline("summarization")
+result = classifier("Paris is the capital and most populous city of France, with an estimated population of 2,175,601 residents as of 2018, in an area of more than 105 square kilometres (41 square miles). The City of Paris is the centre and seat of government of the region and province of Île-de-France, or Paris Region, which has an estimated population of 12,174,880, or about 18 percent of the population of France as of 2017.",
+                    max_length=30,
+                    min_length=10)
+print(result)
