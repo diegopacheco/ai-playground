6096b97e3761a87520938ebbaccd7cde90cb3ab0
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-18 01:28:24 -0700

    PaliGemma

--- pocs/transformers-PaliGemma/src/main.py
+++ pocs/transformers-PaliGemma/src/main.py
@@ -1,13 +1,18 @@
+from PIL import Image
+import requests
 from transformers import AutoProcessor, PaliGemmaForConditionalGeneration
 
 model_id = "google/paligemma-3b-mix-224"
 model = PaliGemmaForConditionalGeneration.from_pretrained(model_id)
 processor = AutoProcessor.from_pretrained(model_id)
 
-prompt = "What is on the flower?"
 image_file = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/bee.jpg?download=true"
 raw_image = Image.open(requests.get(image_file, stream=True).raw)
-inputs = processor(prompt, raw_image, return_tensors="pt")
-output = model.generate(**inputs, max_new_tokens=20)
 
-print(processor.decode(output[0], skip_special_tokens=True)[len(prompt):])
+def predict(prompt, raw_image):
+    inputs = processor(prompt, raw_image, return_tensors="pt")
+    output = model.generate(**inputs, max_new_tokens=20)
+    return processor.decode(output[0], skip_special_tokens=True)[len(prompt):]
+
+print(predict("What is on the flower?", raw_image))
+print(predict("What the color of the flower?", raw_image))
