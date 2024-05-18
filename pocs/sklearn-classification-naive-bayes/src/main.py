373bdf234b27e271276c4483e21fab8d6b7d1662
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-18 01:03:50 -0700

    Naive Bayes

---
+++ pocs/sklearn-classification-naive-bayes/src/main.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+
+# Load the iris dataset
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # we only take the first two features.
+y = iris.target
+
+# Split the data into a training set and a test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create an instance of GaussianNB and fit the data.
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+
+# Plot the decision boundary
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
+                     np.arange(y_min, y_max, 0.02))
+
+Z = gnb.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.contourf(xx, yy, Z, alpha=0.8)
+plt.scatter(X[:, 0], X[:, 1], c=y, edgecolor='k')
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.title('Naive Bayes Decision Boundary')
+plt.show()
