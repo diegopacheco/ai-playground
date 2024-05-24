a17e503a80ab801917a8028c863bd25e24af1f53
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-24 01:15:34 -0700

    .

---
+++ pocs/pytorch-stylegan2/README.md
@@ -0,0 +1,6 @@
+### Result
+* GAN - Generative Adversarial Network
+* StyleGAN2 - Style-based GAN architecture
+
+<img src='' />
+
---
+++ pocs/pytorch-stylegan2/install-deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+/bin/pip install https://github.com/podgorskiy/dnnlib/releases/download/0.0.1/dnnlib-0.0.1-py3-none-any.whl
+/bin/pip install -r requirements.txt
