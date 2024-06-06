804916939f243e44a1175c3ae2a290b52b5cdbdf
diegopacheco <diego.pacheco.it@gmail.com>
2024-06-06 00:36:29 -0700

    xLSTM

---
+++ pocs/transformers-xlstm/src/main.py
@@ -0,0 +1,43 @@
+import torch
+
+from xlstm import (
+    xLSTMBlockStack,
+    xLSTMBlockStackConfig,
+    mLSTMBlockConfig,
+    mLSTMLayerConfig,
+    sLSTMBlockConfig,
+    sLSTMLayerConfig,
+    FeedForwardConfig,
+)
+
+cfg = xLSTMBlockStackConfig(
+    mlstm_block=mLSTMBlockConfig(
+        mlstm=mLSTMLayerConfig(
+            conv1d_kernel_size=4, qkv_proj_blocksize=4, num_heads=4
+        )
+    ),
+    slstm_block=sLSTMBlockConfig(
+        slstm=sLSTMLayerConfig(
+            backend="vanilla",
+            num_heads=4,
+            conv1d_kernel_size=4,
+            bias_init="powerlaw_blockdependent",
+        ),
+        feedforward=FeedForwardConfig(proj_factor=1.3, act_fn="gelu"),
+    ),
+    context_length=256,
+    num_blocks=7,
+    embedding_dim=128,
+    slstm_at=[1],
+
+)
+
+xlstm_stack = xLSTMBlockStack(cfg)
+
+x = torch.randn(4, 256, 128).to("cpu")
+xlstm_stack = xlstm_stack.to("cpu")
+y = xlstm_stack(x)
+y.shape == x.shape
+
+print(f"x.shape: {x.shape}")
+print(f"y.shape: {y.shape}")
