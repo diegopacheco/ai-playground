101e11cd4421eac3fa87cd73ae3dc3894437f87c
diegopacheco <diego.pacheco.it@gmail.com>
2024-06-12 00:25:39 -0700

    .

---
+++ pocs/auto-sklearn-simple/src/main.py
@@ -0,0 +1,13 @@
+import autosklearn.classification
+import sklearn.model_selection
+import sklearn.datasets
+import sklearn.metrics
+
+X, y = sklearn.datasets.load_digits(return_X_y=True)
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, random_state=1)
+
+automl = autosklearn.classification.AutoSklearnClassifier()
+automl.fit(X_train, y_train)
+
+y_hat = automl.predict(X_test)
+print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
