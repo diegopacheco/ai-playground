ed0a403dbe54cb2f8a08d9dcfedfd9fbba747b11
diegopacheco <diego.pacheco.it@gmail.com>
2025-12-13 01:43:39 -0800

    langgraph

---
+++ pocs/langgraph-mock-poc/main.py
@@ -0,0 +1,13 @@
+from langgraph.graph import StateGraph, MessagesState, START, END
+
+def mock_llm(state: MessagesState):
+    return {"messages": [{"role": "ai", "content": "hello world"}]}
+
+graph = StateGraph(MessagesState)
+graph.add_node(mock_llm)
+graph.add_edge(START, "mock_llm")
+graph.add_edge("mock_llm", END)
+graph = graph.compile()
+
+result = graph.invoke({"messages": [{"role": "user", "content": "hi!"}]})
+print(result)
