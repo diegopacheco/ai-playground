5c2ef9286acaa2ce15d72848c723890e731fbb83
diegopacheco <diego.pacheco.it@gmail.com>
2026-03-07 20:13:29 -0800

    added k8s-sre-agent-operator - ui

--- pocs/k8s-sre-agent-operator/ui/src/main.tsx
+++ pocs/k8s-sre-agent-operator/ui/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
 )
