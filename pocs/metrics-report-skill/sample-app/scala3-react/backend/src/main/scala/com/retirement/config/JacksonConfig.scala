26131a61a4abd434ca03569234f9229a565ac1f2
diegopacheco <diego.pacheco.it@gmail.com>
2026-03-31 21:11:50 -0700

    metrics skill

--- pocs/metrics-report-skill/sample-app/scala3-react/backend/src/main/scala/com/retirement/config/JacksonConfig.scala
+++ pocs/metrics-report-skill/sample-app/scala3-react/backend/src/main/scala/com/retirement/config/JacksonConfig.scala
@@ -1,11 +1,10 @@
 package com.retirement.config
 
+import com.fasterxml.jackson.databind.Module
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.springframework.context.annotation.{Bean, Configuration}
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilderCustomizer
 
 @Configuration
 class JacksonConfig:
   @Bean
-  def jacksonScalaModule(): Jackson2ObjectMapperBuilderCustomizer =
-    builder => builder.modules(DefaultScalaModule)
+  def scalaModule(): Module = DefaultScalaModule
