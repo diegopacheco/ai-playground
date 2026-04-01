a31766adc99cd56e4113e2cc6975650d2cd3b8d7
diegopacheco <diego.pacheco.it@gmail.com>
2026-03-31 20:53:34 -0700

    metrics skill

---
+++ pocs/metrics-report-skill/sample-app/django-6-python313/backend/retirement_planner/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for retirement_planner project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'retirement_planner.settings')
+
+application = get_asgi_application()
