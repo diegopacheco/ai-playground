1bd59bf4194a27513a4dd19bcf4eaa14ec5837b3
diegopacheco <diego.pacheco.it@gmail.com>
2026-07-13 03:00:36 -0700

    added desktop-app version

--- pocs/fable-5-game-stand/app.py
+++ pocs/fable-5-game-stand/app.py
@@ -151,4 +151,5 @@ def api_set_config():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5057)
+    port = int(os.environ.get("GAME_STAND_PORT", "5057"))
+    app.run(host="127.0.0.1", port=port)
